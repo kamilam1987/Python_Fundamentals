53f889998d99cf8bb7500de16a19f96eb07fe6fe
kamilam1987 <kamilam1987@gmail.com>
2018-09-27 21:14:01 +0100

    Created program to calculate square

---
+++ squares.py
@@ -0,0 +1,14 @@
+# List of numbers
+#l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# for loop
+# range returns generator(the same as list)
+#for i in l:
+for i in range(20):
+    #2 spaces for value  of i
+    print(f"{i:2} {i*i:4}")
+   # print(i)
+    # square
+    #print(i*i)
+
+print("That's the whole list")
